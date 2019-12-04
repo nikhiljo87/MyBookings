b67b6656a688afaaa335e4b4c0ba4c3f04412a74
nikhiljo87 <nikhildilipjoshi@gmail.com>
2019-12-04 07:13:24 +0530

    nikhil: initial setup for app

---
+++ ui-react/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  render() {
+    return (
+      
+        <div className="container">
+          <h1>Hello bookings</h1>
+        </div>
+      
+    );
+  }
+}
+
+export default App;
