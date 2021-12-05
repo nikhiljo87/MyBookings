a3a14d271a0381b231e65535bfd05f9e687f7c74
UI-Chakki <58452409+nikhiljo87@users.noreply.github.com>
2021-12-05 17:54:25 +0530

    new change
    new chnage

--- ui-react/src/App.js
+++ ui-react/src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import BookingList from './BookingList/BookingList';
 class App extends Component {
   render() {
     return (
-      
-        <div className="container">
-          <h1>Hello bookings</h1>
+      <Router>
+       <div>
+          <nav className="navbar navbar-expand navbar-dark bg-dark">
+            <a href="/bookinglist" className="navbar-brand">
+              Booking List
+            </a>            
+          </nav>
+
+          <div className="container mt-3">
+            <Switch>
+              <Route exact path="/" component={BookingList} />
+            </Switch>
+          </div>
         </div>
-      
-    );
+      </Router>
+    )
   }
 }
 
